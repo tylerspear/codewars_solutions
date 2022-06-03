b87a09fb288296c9fde896c906633f3f7c0a5fee
Tyler Spear <stylerspear@gmail.com>
2022-06-03 10:25:34 -0500

    codewars for 6.3.22

---
+++ 6.3.22_replace_with_alpha_pos.js
@@ -0,0 +1,25 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+    let key = 'abcdefghijklmnopqrstuvwxyz'
+    key = key.split('')
+    
+    let positions = []
+    let letters = text.toLowerCase().split('')
+    letters.forEach((el) => {
+      if(key.includes(el)){
+       positions.push(key.indexOf(el)+1)
+      }
+    })
+    return positions.join(' ')
+  }
