c18571af71ae38006c13c75e1b0ec24d6d31f47c
Tyler Spear <stylerspear@gmail.com>
2022-06-17 09:42:26 -0500

    codewars for 6.17.22

---
+++ 6.17.22_counting_chars.js
@@ -0,0 +1,17 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count (string) {  
+    // The function code should be here
+      let counts = {}
+      let letters = string.split('')
+      letters.forEach((letter)=>{
+        if(counts[letter]){
+          counts[letter] += 1
+        } else {
+          counts[letter] = 1
+        }
+      })
+     return counts;
+  }
